3a9cc030255be5343af03840bcfeb74bef6f5b5d
ran <abbshr@outlook.com>
2015-05-26 10:52:03 +0800

    测试致知楼,改进脚本

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
   ./conn.sh <CMCC-EDU | CMCC-WEB | CMCC>
 ```
 
-** Notice: 目前只知道我工CMCC有这特色, 其他地区尚不明确 **  
-** Notice: 脚本适用于使用NetWorkManager网络管理工具的系统 **  
-** Notice: 不必惊讶原理, 简单到说出来你都不信;) **
+**Notice: 目前只知道我工CMCC有这特色, 其他地区尚不明确**  
+**Notice: 脚本适用于使用NetWorkManager网络sds管理工具的系统**  
+**Notice: 不必惊讶原理, 简单到说出来你都不信;)**
+**Notice: 大前提是你先能连上WI-FI~~**
--- conn.sh
+++ conn.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
+clean_up() {
+  rm /etc/NetworkManager/system-connections/${1}*
+  ref=0
+}
+
+bootstrap() {
+  declare -i ref=$(ll /etc/NetworkManager/system-connections/ | grep ${1}* | wc -l)
+  ((ref >= LIMIT)) && clean_up
+}
+
+declare -i LIMIT=10
+TIMEOUT=15s
+bootstrap
+
 while true; do
   echo "try to connect to $1 ..."
   nmcli dev wifi connect $1 iface wlan0
-  sleep 15s
-  nmcli dev disconnect iface wlan0
+  case $? in
+    0 )
+      sleep 15s
+      nmcli dev disconnect iface wlan0
+    ;;
+    1 )
+      exit 1
+    ;;
+    4 )
+      if ((ref >= LIMIT)) then
+        echo "cleanning connection files ..."
+        clean_up $1
+      else
+        ref+=1
+      fi
+    ;;
+  esac
 done
